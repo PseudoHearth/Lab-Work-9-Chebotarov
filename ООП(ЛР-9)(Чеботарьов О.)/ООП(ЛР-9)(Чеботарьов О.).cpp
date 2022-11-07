c7a449a7b07841b0d95c5a6fb677dd9f53508e60
админ <@DESKTOP-IMGPV8N>
2022-11-07 18:47:14 +0200

    Добавьте файлы проекта.

---
+++ ООП(ЛР-9)(Чеботарьов О.)/ООП(ЛР-9)(Чеботарьов О.).cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Base {
+
+public:
+
+	virtual void myname() {
+		cout << "This is class Base" << endl;
+
+	}
+
+};
+
+class DerA : public Base {
+
+public:
+
+	void myname()
+	{
+		cout << "DerA" << endl;
+	}
+
+};
+
+class DerB :public Base {
+
+public:
+
+	void myname()
+	{
+		cout << "DerB" << endl;
+	}
+
+};
+
+int main()
+{
+	Base one;
+	Base* tmp;
+	DerA two;
+	DerB X;
+	tmp = &one;
+	tmp->myname();
+	tmp = &two;
+	tmp->myname();
+	tmp = &X;
+	tmp->myname();
+
+	return 0;
+
+}
